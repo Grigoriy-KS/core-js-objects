87d447ab5544f4c4ff387776d3077988884ddbde
Grigoriy Ksyonz <KsyonzGS@gmail.com>
2025-03-10 23:58:49 +0300

    feat: add solution to problem

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -32,8 +32,17 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((acc, obj) => {
+    Object.keys(obj).forEach((key) => {
+      if (Object.prototype.hasOwnProperty.call(acc, key)) {
+        acc[key] += obj[key];
+      } else {
+        acc[key] = obj[key];
+      }
+    });
+    return acc;
+  }, {});
 }
 
 /**
